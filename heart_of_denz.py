6fb0e45848eb73e8ec2b29e5943ee5b02930f336
XDawnBreakerX <46450828+XDawnBreakerX@users.noreply.github.com>
2019-01-08 20:21:06 +0530

    Update heart_of_denz.py

--- heart_of_denz.py
+++ heart_of_denz.py
@@ -10,24 +10,6 @@ client = commands.Bot(command_prefix = "d!")
 async def commands(ctx):
     await client.say('```This command shows the help dialogue box! \n The prefix is d! \n ----- The available commands are 1)goal \n ----- This shows how many minutes are left to reach the goal \n ----- 2)hi \n ----- Its simple! it says hi! \n ----- 3)spam \n ----- This command lets u spam text till a limit of 20! syntax: d!spam <text> <no. of times>```')
 
-@client.command(pass_context=True)
-async def hi(ctx):
-    await client.say('Yo Boi <@%s>' % (ctx.message.author.id))
-
-@client.command(name='8ball',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['eight_ball', 'eightball', '8-ball'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'That is a resounding no',
-        'It is not looking likely',
-        'Too hard to tell',
-        'It is quite possible',
-        'Definitely',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 @client.command(pass_context=True)
 async def spam(ctx):
