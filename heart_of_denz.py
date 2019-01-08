f384e028b6bf021bc77c198b9b012ca2f6dfb1bf
XDawnBreakerX <46450828+XDawnBreakerX@users.noreply.github.com>
2019-01-08 19:23:33 +0530

    Update heart_of_denz.py

--- heart_of_denz.py
+++ heart_of_denz.py
@@ -7,6 +7,10 @@ TOKEN = ''
 client = commands.Bot(command_prefix = "d!")
 
 @client.command(pass_context=True)
+async def help(ctx):
+    await client.say('This command shows the help dialogue box! \n The prefix is d! \n \t The available commands are 1)goal \n \t \t This shows how many minutes are left to reach the goal \n \t 2)hi \n \t \t Its simple! it says hi! \n \t 3)spam \n \t \t This command lets u spam text till a limit of 20! syntax: d!spam <text> <no. of times>')
+
+@client.command(pass_context=True)
 async def hi(ctx):
     await client.say('Yo Boi <@%s>' % (ctx.message.author.id))
 
