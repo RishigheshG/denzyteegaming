f19c6603438a469e0886a17ba1b02bf23da97ad1
XDawnBreakerX <46450828+XDawnBreakerX@users.noreply.github.com>
2019-02-12 12:20:34 +0530

    Update heart_of_denz.py

--- heart_of_denz.py
+++ heart_of_denz.py
@@ -52,7 +52,7 @@ async def clear(ctx, amount=100):
 
 @client.command(pass_context=True)
 async def goal(ctx):
-    await client.say('Our goal is to get 240000 minutes of watchtime and 1000 subscribers! \n As of now, We Have 1884 Minutes of watchtime, 30 Subscribers, 1066 Views and 6 videos! <@%s>' % (ctx.message.author.id))
+    await client.say('Our goal is to get 240000 minutes of watchtime and 1000 subscribers! \n As of now, We Have 1930 Minutes of watchtime, 33 Subscribers, 1112 Views and 6 videos! <@%s>' % (ctx.message.author.id))
 
 @client.command(pass_context=True)
 async def denzytee(ctx):
